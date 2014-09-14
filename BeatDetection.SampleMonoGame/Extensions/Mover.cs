3e100e7264a95e204293c706aa1feb3bd7626382
Jwhee <jerrythefob@gmail.com>
2014-09-14 09:35:22 -0700

    Add Mover and Transform

---
+++ BeatDetection.SampleMonoGame/Extensions/Mover.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Xna.Framework.Extensions
+{
+  public abstract class Mover
+  {
+    private Transform transform;
+
+    public Mover(Transform transform)
+    {
+      this.transform = transform;
+    }
+
+    public abstract void Update(GameTime gametime);
+  }
+}
---
+++ BeatDetection.SampleMonoGame/Extensions/Transform.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.Xna.Framework.Extensions
+{
+  public class Transform
+  {
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Rotation { get; set; }
+    public float Size { get; set; }
+  }
+}
