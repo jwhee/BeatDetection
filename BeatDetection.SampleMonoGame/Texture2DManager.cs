680c43457294e0897e711f4aa5977e6e8f43392e
Jwhee <jerrythefob@gmail.com>
2014-09-13 20:26:51 -0700

    Add texture slicer

--- BeatDetection.SampleMonoGame/Texture2DManager.cs
+++ BeatDetection.SampleMonoGame/Texture2DManager.cs
@@ -4,9 +4,9 @@
   using System.Collections.Generic;
   using System.IO;
 
+  using Microsoft.Xna.Framework;
   using Microsoft.Xna.Framework.Graphics;
-  using Color = Microsoft.Xna.Framework.Color;
-
+  
   public class Texture2DManager
   {
     private static readonly Lazy<Texture2DManager> instance
@@ -14,6 +14,7 @@
 
     private GraphicsDevice graphics;
     private Dictionary<string, Texture2D> textures;
+    private Dictionary<string, List<Texture2D>> slicedTextures;
     private BlendState blendColor = null;
     private BlendState blendAlpha = null;
     private SpriteBatch spriteBatch = null;
@@ -21,6 +22,7 @@
     private Texture2DManager()
     {
       this.textures = new Dictionary<string, Texture2D>();
+      this.slicedTextures = new Dictionary<string, List<Texture2D>>();
     }
 
     public Texture2DManager Initialize(GraphicsDevice graphics)
@@ -50,7 +52,69 @@
       return this;
     }
 
-    public Texture2DManager Load(string key, string path)
+    public Texture2DManager Slice(string key, int row, int col)
+    {
+      var original = this.textures[key];
+
+      if (row == 0)
+      {
+        row = 1;
+      }
+
+      if (col == 0)
+      {
+        col = 1;
+      }
+
+      var width = original.Width / row;
+      var height = original.Height / col;
+      
+      using (var renderTarget = new RenderTarget2D(this.graphics, width, height))
+      {
+        var sliced = new List<Texture2D>();
+        for (int i = 0; i < col; i++ )
+        {
+          for (int j = 0; j < row; j++ )
+          {
+            var texture = new Texture2D(this.graphics, width, height);
+            var sourceRectangle = new Rectangle(j * width, i * height, width, height);
+
+            //Setup a render target to hold our final texture which will have premulitplied alpha values
+            this.graphics.SetRenderTarget(renderTarget);
+            this.graphics.Clear(Color.Black);
+
+            // Multiply each color by the source alpha, and write in just the color values into the final texture
+            this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendColor);
+            this.spriteBatch.Draw(original, Vector2.Zero, sourceRectangle, Color.White);
+            this.spriteBatch.End();
+
+            // Now copy over the alpha values from the PNG source texture to the final one, without multiplying them
+            this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendAlpha);
+            this.spriteBatch.Draw(original, Vector2.Zero, sourceRectangle, Color.White);
+            this.spriteBatch.End();
+
+            //Release the GPU back to drawing to the screen
+            graphics.SetRenderTarget(null);
+
+            // Store data from render target because the RenderTarget2D is volatile
+            Color[] data = new Color[texture.Width * texture.Height];
+            renderTarget.GetData(data);
+
+            // Unset texture from graphic device and set modified data back to it
+            this.graphics.Textures[0] = null;
+            texture.SetData(data);
+
+            sliced.Add(texture);
+          }
+        }
+
+        this.slicedTextures[key] = sliced;
+      }
+
+      return this;
+    }
+
+    public Texture2DManager Load(string key, string path, bool preMultiplyAlpha = true)
     {
       Texture2D texture = null;
       using (var stream = File.OpenRead(path))
@@ -58,32 +122,35 @@
         texture = Texture2D.FromStream(this.graphics, stream);
       }
 
-      using (var renderTarget = new RenderTarget2D(this.graphics, texture.Width, texture.Height))
+      if (preMultiplyAlpha)
       {
-        //Setup a render target to hold our final texture which will have premulitplied alpha values
-        this.graphics.SetRenderTarget(renderTarget);
-        this.graphics.Clear(Color.Black);
+        using (var renderTarget = new RenderTarget2D(this.graphics, texture.Width, texture.Height))
+        {
+          // Setup a render target to hold our final texture which will have premulitplied alpha values
+          this.graphics.SetRenderTarget(renderTarget);
+          this.graphics.Clear(Color.Black);
 
-        //Multiply each color by the source alpha, and write in just the color values into the final texture
-        this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendColor);
-        this.spriteBatch.Draw(texture, texture.Bounds, Color.White);
-        this.spriteBatch.End();
+          // Multiply each color by the source alpha, and write in just the color values into the final texture
+          this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendColor);
+          this.spriteBatch.Draw(texture, texture.Bounds, Color.White);
+          this.spriteBatch.End();
 
-        //Now copy over the alpha values from the PNG source texture to the final one, without multiplying them
-        this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendAlpha);
-        this.spriteBatch.Draw(texture, texture.Bounds, Color.White);
-        this.spriteBatch.End();
+          // Now copy over the alpha values from the PNG source texture to the final one, without multiplying them
+          this.spriteBatch.Begin(SpriteSortMode.Immediate, this.blendAlpha);
+          this.spriteBatch.Draw(texture, texture.Bounds, Color.White);
+          this.spriteBatch.End();
 
-        //Release the GPU back to drawing to the screen
-        graphics.SetRenderTarget(null);
+          // Release the GPU back to drawing to the screen
+          this.graphics.SetRenderTarget(null);
 
-        // Store data from render target because the RenderTarget2D is volatile
-        Color[] data = new Color[texture.Width * texture.Height];
-        renderTarget.GetData(data);
+          // Store data from render target because the RenderTarget2D is volatile
+          Color[] data = new Color[texture.Width * texture.Height];
+          renderTarget.GetData(data);
 
-        // Unset texture from graphic device and set modified data back to it
-        graphics.Textures[0] = null;
-        texture.SetData(data);
+          // Unset texture from graphic device and set modified data back to it
+          this.graphics.Textures[0] = null;
+          texture.SetData(data);
+        }
       }
 
       this.textures[key] = texture;
@@ -93,7 +160,7 @@
 
     public Texture2DManager CreateSquare(string key)
     {
-      var square = new Texture2D(graphics, 2, 2);
+      var square = new Texture2D(this.graphics, 2, 2);
       Color[] data = new Color[4];
       for (int i = 0; i < data.Length; ++i) data[i] = Color.White;
       square.SetData(data);
@@ -111,6 +178,11 @@
     public Texture2D Get(string key)
     {
       return this.textures[key];
+    }
+
+    public Texture2D Get(string key, int index)
+    {
+      return this.slicedTextures[key][index];
     }
 
     public Texture2D this[string key]
