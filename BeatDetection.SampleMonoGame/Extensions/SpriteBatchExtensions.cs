5101a5100915ae9d6510b34bfd0f8eb3113ba9ea
Jwhee <jerrythefob@gmail.com>
2014-09-15 23:28:06 -0700

    Add SpriteBatch extensions to Draw()

---
+++ BeatDetection.SampleMonoGame/Extensions/SpriteBatchExtensions.cs
@@ -0,0 +1,61 @@
+﻿namespace Microsoft.Xna.Framework.Extensions
+{
+  using Microsoft.Xna.Framework.Graphics;
+
+  public static class SpriteBatchExtensions
+  {
+    public static void Draw(
+      this SpriteBatch spriteBatch,
+      Texture2D texture,
+      float xPos,
+      float yPos,
+      float rotation,
+      float scale,
+      Rectangle? sourceRectangle = null,
+      Color? color = null,
+      Vector2? origin = null)
+    {
+      var colorParam = Color.White;
+      if (color.HasValue)
+      {
+        colorParam = color.Value;
+      }
+
+      var originParam = new Vector2(texture.Width / 2, texture.Height / 2);
+      if (origin.HasValue)
+      {
+        originParam = origin.Value;
+      }
+
+      spriteBatch.Draw(
+        texture,
+        new Vector2(xPos, yPos),
+        sourceRectangle,
+        colorParam,
+        rotation,
+        originParam,
+        scale,
+        SpriteEffects.None,
+        0.0f);
+    }
+
+    public static void Draw(
+      this SpriteBatch spriteBatch,
+      Texture2D texture,
+      Transform transform,
+      Rectangle? sourceRectangle = null,
+      Color? color = null,
+      Vector2? origin = null)
+    {
+      spriteBatch.Draw(
+        texture, 
+        transform.X, 
+        transform.Y,
+        transform.Rotation,
+        transform.Size,
+        sourceRectangle,
+        color,
+        origin);
+    }
+  }
+}
